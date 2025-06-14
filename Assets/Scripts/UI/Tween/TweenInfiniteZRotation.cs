6572e50e6431c72df114069c900b42f72cf6240e
OkanSHome <okans@gamegos.com>
2025-06-14 19:53:10 +0300

    Tween scripts added

---
+++ Assets/Scripts/UI/Tween/TweenInfiniteZRotation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace UI.Tween
+{
+    public class TweenInfiniteZRotation : MonoBehaviour
+    {
+        [SerializeField] private float _duration = 3f;
+
+        private void Start()
+        {
+            transform.DOLocalRotate(new Vector3(0, 0, 360f), _duration, RotateMode.FastBeyond360)
+                .SetEase(Ease.Linear)
+                .SetLoops(-1, LoopType.Restart);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Tween/TweenScaleUpDown.cs
@@ -0,0 +1,20 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace UI.Tween
+{
+    public class TweenScaleUpDown : MonoBehaviour
+    {
+        [SerializeField] private float _scaleUpSize = 1.1f;
+        [SerializeField] private float _duration = 0.7f;
+        private Vector3 startScale;
+        private void Start()
+        {
+            startScale = transform.localScale;
+            var sequence = DOTween.Sequence();
+            sequence.Append(transform.DOScale(startScale * (_scaleUpSize), _duration).SetEase(Ease.Linear));
+            sequence.Append(transform.DOScale(startScale, _duration).SetEase(Ease.Linear));
+            sequence.SetLoops(-1, LoopType.Restart);
+        }
+    }
+}
