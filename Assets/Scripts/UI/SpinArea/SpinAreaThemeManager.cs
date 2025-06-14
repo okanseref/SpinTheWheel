33700e536a4cb99948229bae327d2e95cf4ce3f9
OkanSHome <okans@gamegos.com>
2025-06-14 19:06:11 +0300

    SpinArea theme added

--- Assets/Scripts/UI/SpinArea/SpinAreaThemeManager.cs
+++ Assets/Scripts/UI/SpinArea/SpinAreaThemeManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Utils;
+using Zone;
 
 public class SpinAreaThemeManager : Singleton<SpinAreaThemeManager>
 {
@@ -11,9 +12,38 @@ public class SpinAreaThemeManager : Singleton<SpinAreaThemeManager>
     [SerializeField] private Image SpinAreaBaseImage;
     [SerializeField] private TextMeshProUGUI SpinAreaTitleText;
     [SerializeField] private TextMeshProUGUI SpinAreaBonusText;
+    [SerializeField] private Sprite SpinBronzeBase;
+    [SerializeField] private Sprite SpinBronzeCursor;
+    [SerializeField] private Sprite SpinSilverBase;
+    [SerializeField] private Sprite SpinSilverCursor;
+    [SerializeField] private Sprite SpinGoldenBase;
+    [SerializeField] private Sprite SpinGoldenCursor;
 
-    public void LoadSpinArea()
+    public void LoadSpinArea(ZoneType zoneType)
     {
         //
+        switch (zoneType)
+        {
+            case ZoneType.Basic:
+                SpinAreaCursorImage.sprite = SpinBronzeCursor;
+                SpinAreaBaseImage.sprite = SpinBronzeBase;
+                SpinAreaTitleText.text = "SPIN";
+                SpinAreaBonusText.text = "";
+                break;
+            case ZoneType.Silver:
+                SpinAreaCursorImage.sprite = SpinSilverCursor;
+                SpinAreaBaseImage.sprite = SpinSilverBase;
+                SpinAreaTitleText.text = "SILVER SPIN";
+                SpinAreaBonusText.text = "Safe Spin!";
+                SpinAreaBonusText.color = Color.green;
+                break;
+            case ZoneType.Golden:
+                SpinAreaCursorImage.sprite = SpinGoldenCursor;
+                SpinAreaBaseImage.sprite = SpinGoldenBase;
+                SpinAreaTitleText.text = "GOLDEN SPIN";
+                SpinAreaBonusText.text = "Up To x10 Rewards!";
+                SpinAreaBonusText.color = Color.yellow;
+                break;
+        }
     }
 }
