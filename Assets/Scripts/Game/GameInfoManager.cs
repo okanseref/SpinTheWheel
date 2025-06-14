3c4ff89af05af7276115bfbb4a90ac296c626ab5
OkanSHome <okans@gamegos.com>
2025-06-14 16:35:38 +0300

    Game & GameInfoManager added

---
+++ Assets/Scripts/Game/GameInfoManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Utils;
+using Zone;
+
+namespace Game
+{
+    public class GameInfoManager : Singleton<GameInfoManager>
+    {
+        public ZoneSettingsCollection ZoneSettingsCollection { get; private set; }
+        public SpecialZoneSettings SpecialZoneSettings { get; private set; }
+        
+        private void Awake()
+        {
+            ZoneSettingsCollection = Resources.Load<ZoneSettingsCollection>("ScriptableObjects/ZoneSettingsCollection");
+            SpecialZoneSettings = Resources.Load<SpecialZoneSettings>("ScriptableObjects/SpecialZoneSettings");
+        }
+
+        public ZoneSettings GetZoneLooped(int zoneIndex)
+        {
+            zoneIndex %= ZoneSettingsCollection.ZoneSettingsList.Count;
+            return ZoneSettingsCollection.ZoneSettingsList[zoneIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/Game/GameStateManager.cs
@@ -0,0 +1,43 @@
+﻿using UI.SpinArea;
+using UI.SpinArea.Signal;
+using UnityEngine;
+using Utils;
+using Zone;
+
+namespace Game
+{
+    public class GameStateManager : MonoBehaviour
+    {
+        private int _currentZone = 0;
+        private int _rollResult = -1;
+        private ZoneSettings _currentZoneSettings;
+        private void Start()
+        {
+            PrepareForSpin();
+        }
+
+        private void PrepareForSpin()
+        {
+            var currentZone = GameInfoManager.Instance.GetZoneLooped(_currentZone);
+            
+            SpinAreaSlotManager.Instance.Init(currentZone);
+            SpinAreaThemeManager.Instance.LoadSpinArea();
+            
+            SignalBus.Instance.Subscribe<SpinClickedSignal>(StartSpin);
+        }
+
+        private void StartSpin()
+        {
+            SignalBus.Instance.Unsubscribe<SpinClickedSignal>(StartSpin);
+
+            _rollResult = Random.Range(0, 8);
+            SpinAreaRollManager.Instance.StartRoll(_rollResult, OnRollComplete);
+        }
+
+        private void OnRollComplete()
+        {
+            // Give rewards
+            PrepareForSpin();
+        }
+    }
+}
