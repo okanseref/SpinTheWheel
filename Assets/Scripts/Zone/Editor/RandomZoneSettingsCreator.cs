7f4e5e0f09b75b712bc686361fa31c3e64abab3d
OkanSHome <okans@gamegos.com>
2025-06-16 23:02:51 +0300

    Editor zone creator improved

--- Assets/Scripts/Zone/Editor/RandomZoneSettingsCreator.cs
+++ Assets/Scripts/Zone/Editor/RandomZoneSettingsCreator.cs
@@ -21,7 +21,8 @@ public class RandomZoneSettingsCreator
             {
                 zoneSettings.Rewards.Add(GetRandomExchangeData());            
             }
-            
+
+            zoneSettings.BombIndex = Random.Range(0, 8);
             ZoneSettingsList.Add(zoneSettings);
         }
 
@@ -36,6 +37,19 @@ public class RandomZoneSettingsCreator
 
     private static ExchangeData GetRandomExchangeData()
     {
-        return new ExchangeData(Random.Range(1, Enum.GetNames(typeof(ExchangeType)).Length + 1), 1, 1);
+        var type = Random.Range(1, Enum.GetNames(typeof(ExchangeType)).Length + 1);
+        var subType = 1;
+
+        var value = type switch
+        {
+            (int)ExchangeType.Currency => Random.Range(1, 10) * 1000,
+            (int)ExchangeType.WeaponProgress => Random.Range(1, 10),
+            (int)ExchangeType.Case => Random.Range(1, 3),
+            _ => 1
+        };
+
+        var subTypeCount = Resources.LoadAll<Sprite>("ExchangeVisuals/" + (ExchangeType)type).Length;
+        subType = Random.Range(1, subTypeCount + 1);
+        return new ExchangeData(type, subType, value);
     }
 }
