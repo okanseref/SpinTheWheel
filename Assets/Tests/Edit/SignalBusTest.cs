2f652911564187a1d4456f968cc55994d49c38b9
OkanSHome <okans@gamegos.com>
2025-06-17 00:03:03 +0300

    SignalBus unit test added

---
+++ Assets/Tests/Edit/SignalBusTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using Game;
+using Game.Signal;
+using NUnit.Framework;
+using Unity.EditorCoroutines.Editor;
+using UnityEngine;
+using Utils;
+using Zone;
+
+namespace Tests.Edit
+{
+    public class SignalBusTest : MonoBehaviour
+    {
+        private int _counter = 0;
+        [Test]
+        public void SilverZoneTest()
+        {
+            SignalBus.Instance.Subscribe<GameResetSignal>(OnSignal);
+            SignalBus.Instance.Fire(new GameResetSignal());
+            SignalBus.Instance.Unsubscribe<GameResetSignal>(OnSignal);
+            SignalBus.Instance.Fire(new GameResetSignal());
+            Debug.Log("Result: " + _counter + "\nExpected: " + 1);
+            Assert.AreEqual(_counter == 1, true);
+        }
+
+        private void OnSignal()
+        {
+            _counter++;
+        }
+    }
+}
